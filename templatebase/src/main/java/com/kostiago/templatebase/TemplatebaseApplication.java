e35160b3579e60ef1f4b34fc60cc83da2b725384
kostiago <tiago_pb-@hotmail.com>
2025-04-09 10:07:09 -0300

    feat: criando a estrutura base de desafios

---
+++ templatebase/src/main/java/com/kostiago/templatebase/TemplatebaseApplication.java
@@ -0,0 +1,13 @@
+package com.kostiago.templatebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplatebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplatebaseApplication.class, args);
+	}
+
+}
---
+++ templatebase/src/test/java/com/kostiago/templatebase/TemplatebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kostiago.templatebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplatebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
